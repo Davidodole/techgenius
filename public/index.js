21e59ad434c000670b2d7f2eb617834308abf09b
Davidodole <dola7468@gmail.com>
2024-08-19 18:40:32 +0100

    updating files

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const button = document.querySelector("nav button"),
+const button = document.getElementById("btn"),
         main = document.getElementById("main"),
         navbar = document.getElementById("navbar"),
         close = document.getElementById("close"),
@@ -8,15 +8,23 @@ const button = document.querySelector("nav button"),
         // btn = document.getElementById("submit"),
         // popup = document.querySelector(".popup"),
         scroll = document.getElementById("scroll"),
-        cards = document.querySelectorAll(".card_animate")
-        body = document.body,
+        btnIcon = document.querySelectorAll("bar")
+        cards = document.querySelectorAll(".card_animate"),
         scroll_animation = document.querySelectorAll("h1");
+
 
   //Adding Evenlistener to the variables declear
 
 button.addEventListener("click",()=>{
     navbar.classList.toggle("open");
 });
+document.addEventListener("click", function (e) {
+  if (!navbar.contains(e.target) && e.target !== button) {
+    navbar.classList.remove("open");
+    
+  }
+});
+
 user.addEventListener("click", ()=>{
     loginUser.classList.add("show")
 });
@@ -45,3 +53,7 @@ scroll_animation.forEach(animate=>{
 cards.forEach(card=>{
     observer.observe(card)
 })
+
+
+
+
